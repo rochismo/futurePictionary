8720a22270d2e38e7fcb23f2e1ca3895ab2793ee
Benjamin <alexis.platino@gmail.com>
2018-07-09 03:18:53 +0200

    Second commit

---
+++ app.js
@@ -0,0 +1,56 @@
+﻿const http = require('http');
+const express = require('express');
+const app = express();
+const socket = require("socket.io");
+const nicknames = [];
+
+const port = 8080;
+
+const handler = () => {
+    console.log("Listening on port: " + port);
+}
+const server = app.listen(port, handler);
+
+
+// Static files
+
+app.use(express.static('public'));
+
+// Socket setup
+
+const io = socket(server);
+
+const handleConnection = (socket) => {
+    console.log("Connection with: " + socket.id);
+
+    // Handle chat event
+    socket.on('chat', function (data) {
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function (data) {
+        socket.broadcast.emit('typing', data);
+    });
+
+    // Handle emptying the feedback
+    socket.on('empty', function () {
+        console.log("Deleting stuff");
+        socket.broadcast.emit('empty', null);
+    });
+
+    socket.on("login", function (data) {
+        socket.nickname = data
+        nicknames.push(socket.nickname);
+        io.sockets.emit('usernames', nicknames);
+    })
+
+    socket.on('disconnect', function () {
+        if (!socket.nickname) return;
+        nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+        io.sockets.emit('usernames', nicknames);
+    })
+};
+
+
+io.on('connection', handleConnection);
---
+++ public/javascripts/chat.js
@@ -0,0 +1,88 @@
+﻿window.onload = function () {
+    // Establish connection
+    const socket = io.connect('http://localhost:8080');
+
+    // Save DOM elements
+    const msg = document.querySelector("#message"),
+        name = document.querySelector("#handle"),
+        sendBtn = document.querySelector("#send"),
+        output = document.querySelector("#output"),
+        feedback = document.querySelector("#feedback"),
+        chat = document.querySelector("#mario-chat"),
+        users = document.querySelector("#sidebar");
+
+    const uses = getUses(socket, name, msg, feedback, output, users);
+    name.value = uses.handleChatLoad(null) || "No name";
+    name.disabled = true;
+
+    // Emit events
+    sendBtn.onclick = uses.handleClicks;
+
+    msg.onkeydown = uses.detectTyping;
+
+    window.onkeypress = uses.handleKeys;
+
+    // Listen for events
+
+    socket.on('chat', uses.retrieveMessages);
+
+    socket.on("typing", uses.handleTyping);
+
+    socket.on("empty", uses.handleEmpty);
+
+    socket.on("usernames", uses.handleUsers);
+}
+
+Window.prototype.getUses = (socket, name, msg, feedback, output, users) => {
+    return {
+        handleChatLoad: (ev) => {
+            const name = prompt("Input your nickname");
+            socket.emit("login", name || "Anonymous");
+            return name;
+        },
+
+        handleClicks: (evt) => {
+            socket.emit('chat', {
+                message: msg.value,
+                name: name.value
+            });
+            msg.value = "";
+        },
+
+        detectTyping: (ev) => {
+            socket.emit('typing', name.value);
+            if (msg.value == "") {
+                socket.emit('empty');
+            }
+        },
+
+        handleTyping: (data) => {
+            feedback.innerHTML = "<p><em>" + data + " is typing . . . </em></p>";
+        },
+
+        handleEmpty: () => {
+            feedback.innerHTML = "";
+        },
+
+        retrieveMessages: (data) => {
+            feedback.innerHTML = "";
+            output.innerHTML += "<p><strong>" + data.name + ":</strong>" + data.message + "</p>";
+        },
+
+        handleKeys: (ev) => {
+            if (ev.keyCode === 13 && msg.value) {
+                socket.emit('chat', {
+                    message: msg.value,
+                    name: name.value
+                });
+                msg.value = "";
+            }
+        },
+
+        handleUsers: (nicknames) => {
+            let html = "";
+            nicknames.forEach((user) => html += "<p>" + user + "<br>"); 
+            users.innerHTML = html;
+        }
+    };
+};
